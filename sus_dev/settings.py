ee6cab1ab1e75f0580911e014965c6f720848c1d
shubham <shubhamsain9640@gmail.com>
2025-04-04 21:52:06 +0530

    new settings

--- sus_dev/settings.py
+++ sus_dev/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -119,11 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATICFILES_DIRS=(os.path.join(BASE_DIR,"static"),)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
  # Needed for collectstatic
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
